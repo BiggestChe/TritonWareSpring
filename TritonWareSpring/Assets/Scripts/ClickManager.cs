788a067a2dd489e31962e33e16e7fde177d18239
Jimmy Che <j2che@ucsd.edu>
2025-04-07 23:39:17 -0700

    .

--- TritonWareSpring/Assets/Scripts/ClickManager.cs
+++ TritonWareSpring/Assets/Scripts/ClickManager.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//basically, next to every class made, put Clickable_Interface after MonoBehavior
+//contains logic of the ClickManager
+//assign whatever object a box collider, give it a script, and include Click()
+//then assign whatever logic you want when clicked on
 public class ClickManager : MonoBehaviour, Clickable_Interface
 {
     private Camera cam;
--- TritonWareSpring/Assets/Scripts/Clickable_Interface.cs
+++ TritonWareSpring/Assets/Scripts/Clickable_Interface.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//basically, next to every class made, put Clickable_Interface after MonoBehavior
+//contains logic of the ClickManager
+//assign whatever object a box collider, give it a script, and include Click()
+//then assign whatever logic
 public interface Clickable_Interface
 {
      void Click(){
